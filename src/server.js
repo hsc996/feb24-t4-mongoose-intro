2c52d6238b96ad7acc97afbbfa4f01fa87fffaf5
hsc996 <han.scaife@gmail.com>
2024-10-29 20:39:19 +1100

    feat: post model implemented

--- src/server.js
+++ src/server.js
@@ -7,7 +7,14 @@ app.use(express.urlencoded({extrended:true}));
 
 //app.verb(path, callback);
 app.get("/", (request, response) => {
-    response.send("Hello, world!");
+    // response.send("<h1>Hello, world!</h1>");
+
+    response.json({
+        message: "Hello world"
+    });
 });
 
 module.exports = { app }
+
+// Server app configuration is finished by this point
+// Export the app so that other files can control when the server
